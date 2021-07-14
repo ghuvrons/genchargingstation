67472cd9b6a548917d3f701cfd79a52ff88b8513
Mohammad Gupron <ghuvrons@gmail.com>
2021-07-14 14:58:21 +0700

    decrypt data

--- data.go
+++ data.go
@@ -4,28 +4,31 @@ import "fmt"
 
 type ChargingData struct {
 	StationID string
-	BatteryID string
+	Hole      uint8
+	BatteryID uint32
 	Volt      float32
 	Curr      float32
 	Temp      float32
 	Capc      float32
-	SOC       float32
-	SOH       float32
+	SOC       int
+	SOH       int
 	Cycle     int
 	Error     uint8
+	LocLong   float64
+	LocLat    float64
 }
 
 func (data *ChargingData) ToString() string {
 	var g string = fmt.Sprintf(`
 ==== Charging Data ====
 StationID : %s
-BatteryID : %s
+BatteryID : %d
 Volt      : %f
 Curr      : %f
 Temp      : %f
 Capc      : %f
-SOC       : %f
-SOH       : %f
+SOC       : %d
+SOH       : %d
 Cycle     : %d
 Error     : %d
 	`, data.StationID, data.BatteryID, data.Volt, data.Curr, data.Temp, data.Capc, data.SOC, data.SOH,
--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package genchargingstation
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,30 +12,37 @@ import (
 type Handler func(data *ChargingData)
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	body, err := ioutil.ReadAll(req.Body)
+	var grad byte = 2
+
+	encBody, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		log.Printf("Error reading body: %v", err)
 		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
-
+	body := make([]byte, len(encBody))
+	for i, v := range encBody {
+		body[i] = v - grad
+	}
+	// trueData := "1,7456,54.2,10.2,58.92,12.2,77,83,423,1,-7.433639308019364,112.69351752859586"
 	datas := strings.Split(string(body), ",")
-	if len(datas) < 10 {
+	if len(datas) < 12 {
 		log.Printf("Error reading body")
 		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
 
-	cData := &ChargingData{
-		StationID: datas[0],
-		BatteryID: datas[1],
-	}
+	cData := &ChargingData{}
 
 	var (
 		tmpF  float64
 		tmpI  int64
 		tmpUi uint64
 	)
+	tmpI, _ = strconv.ParseInt(datas[0], 10, 32)
+	cData.Hole = uint8(tmpI)
+	tmpI, _ = strconv.ParseInt(datas[1], 10, 32)
+	cData.BatteryID = uint32(tmpI)
 	tmpF, _ = strconv.ParseFloat(datas[2], 32)
 	cData.Volt = float32(tmpF)
 	tmpF, _ = strconv.ParseFloat(datas[3], 32)
@@ -43,14 +51,20 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	cData.Temp = float32(tmpF)
 	tmpF, _ = strconv.ParseFloat(datas[5], 32)
 	cData.Capc = float32(tmpF)
-	tmpF, _ = strconv.ParseFloat(datas[6], 32)
-	cData.SOC = float32(tmpF)
-	tmpF, _ = strconv.ParseFloat(datas[7], 32)
-	cData.SOH = float32(tmpF)
+	tmpI, _ = strconv.ParseInt(datas[6], 10, 32)
+	cData.SOC = int(tmpI)
+	tmpI, _ = strconv.ParseInt(datas[7], 10, 32)
+	cData.SOH = int(tmpI)
 	tmpI, _ = strconv.ParseInt(datas[8], 10, 32)
 	cData.Cycle = int(tmpI)
 	tmpUi, _ = strconv.ParseUint(datas[9], 10, 8)
 	cData.Error = uint8(tmpUi)
+	tmpF, _ = strconv.ParseFloat(datas[10], 32)
+	cData.LocLong = tmpF
+	tmpF, _ = strconv.ParseFloat(datas[11], 32)
+	cData.LocLat = tmpF
+	cData.StationID = fmt.Sprintf("%f, %f", cData.LocLong, cData.LocLat)
 
+	fmt.Fprintf(w, "SUCCESS")
 	h(cData)
 }
